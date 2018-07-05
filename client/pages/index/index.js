070206fa9981aea6d847d0842f288f9174d11ccf
dylanzhang123456 <997604787@qq.com>
2018-07-05 11:52:55 -0700

    add pages

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -3,58 +3,23 @@ const config = require('../../config');
 
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
     imgUrls: [
       './ads.png',
       './ads.png',
       './ads.png'
     ],
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    autoplay: true,//是否自动切换
+    indicatorDots: true,//是否显示圆点
+    interval: 5000,//自动切换间隔
+    duration: 1000, //滑动动画时长
+    circular: true  //是否采用衔接滑动
+    //其中只可放置<swiper-item/>组件，否则会导致未定义的行为。
   },
-
-  onLoad: function () {
-    // 查看是否授权
-    wx.getSetting({
-      success: function (res) {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-          wx.getUserInfo({
-            success: function (res) {
-              console(res.userInfo)
-            }
-          })
-        }
-      }
+  imageLoad: function () {
+    //bindload 图片加载的时候自动设定宽度
+    this.setData({
+      imageWidth: wx.getSystemInfoSync().windowWidth-10
     })
-  },
-  bindGetUserInfo: function (e) {
-    console.log(e.detail.userInfo)
-  },
-  requestDemo: function() {
-    wx.getUserInfo({
-      success: function(res) {
-        util.showSuccess(res.userInfo);
-        console.log(res.userInfo);
-      }
-    })
-    console.log(wx.getSystemInfoSync());
-    // wx.request({
-    //   url: `${config.service.host}/weapp/demo`, //仅为示例，并非真实的接口地址,
-    //   method: "GET",
-    //   data: {
-    //     x: '',
-    //     y: ''
-    //   },
-    //   header: {
-    //     'content-type': 'application/json' // 默认值
-    //   },
-    //   success: function(res) {
-    //     util.showSuccess(res.data);
-    //   }
-    // })
-
   }
 })
